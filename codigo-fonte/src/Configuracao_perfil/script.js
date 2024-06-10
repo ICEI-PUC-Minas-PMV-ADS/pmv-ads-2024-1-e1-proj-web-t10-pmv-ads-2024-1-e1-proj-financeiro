c51d9e5d1e03819219f642677b04678aab6069d6
ancarolna <166072620+ancarolna@users.noreply.github.com>
2024-06-09 22:31:02 -0300

    Atualizar o script.js

--- codigo-fonte/src/Configuracao_perfil/script.js
+++ codigo-fonte/src/Configuracao_perfil/script.js
@@ -1,25 +1,49 @@
-     document.getElementById('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var usuario =
-    document.getElementById('usuario').value;
-    var senhaAntiga = 
-    document.getElementById('senhaAntiga').value;
-    var novaSenha =
-    document.getElementById('novaSenha').value;
-    if (!usuario) {
-    alert('Preencha este campo');
-    return;}
-    if (!senhaAntiga) {
-    alert('Preencha com sua senha antiga corretamente');
-    return;}
-    if (!novaSenha) {
-    alert('Crie uma nova senha');
-    return; }
-    if (senhaAntiga === novaSenha) {
-    alert('As senhas devem ser diferentes');
-    return; }
-    localStorage.setItem('usuario', usuario);
-    localStorage.setItem('senhaAntiga', senhaAntiga);
-    localStorage.setItem('novaSenha', novaSenha);
-    alert('A senha foi atualizada com sucesso!');
-    });
+document.getElementById('form').addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            var usuario = document.getElementById('usuario').value;
+            var senhaAntiga = document.getElementById('senhaAntiga').value;
+            var novaSenha = document.getElementById('novaSenha').value;
+
+            if (!usuario) {
+                alert('Preencha o campo de usuário');
+                return;
+            }
+            if (!senhaAntiga) {
+                alert('Preencha o campo de senha antiga corretamente');
+                return;
+            }
+            if (!novaSenha) {
+                alert('Crie uma nova senha');
+                return;
+            }
+            if (senhaAntiga === novaSenha) {
+                alert('As senhas devem ser diferentes');
+                return;
+            }
+
+            var usuariosTexto = localStorage.getItem("usuarios");
+            if (!usuariosTexto) {
+                alert('Nenhum usuário cadastrado.');
+                return;
+            }
+
+            var usuariosObjeto = JSON.parse(usuariosTexto);
+
+            var usuarioEncontrado = usuariosObjeto.find(usuarioObj => usuarioObj.usuario === usuario);
+
+            if (!usuarioEncontrado) {
+                alert('Usuário não existente.');
+                return;
+            }
+
+            if (usuarioEncontrado.senha !== senhaAntiga) {
+                alert('Senha antiga incorreta.');
+                return;
+            }
+
+            usuarioEncontrado.senha = novaSenha;
+            localStorage.setItem("usuarios", JSON.stringify(usuariosObjeto));
+
+            alert('A senha foi atualizada com sucesso!');
+        });
